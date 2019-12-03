8ed180177fcf57659fb289d984a0dc81d18cfe98
qtgeo1248 <gzhou00@stuy.edu>
2019-12-03 15:24:11 -0500

    inputs

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include "shells.h"
 
 int main() {
-    
+    char *test;
+    char buf[1000];
+    printf("Please input a program (for example, \"ls -l -a\"):\n");
+    fgets(buf, 1000, stdin);
     return 0;
 }
