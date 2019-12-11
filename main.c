417bc4d8370ba535a8021d5256f51beca9cf2157
grace-mao23 <gmao00@stuy.edu>
2019-12-10 20:31:08 -0500

    < fixed

--- main.c
+++ main.c
@@ -10,21 +10,19 @@ int main() {
     int *semicolons = &x;
     char dir[1000] = ".";
     char buf[1000] = "\0";
+    // WHILE NOT EXIT
     while (strcmp(token, "exit") != 0 && strcmp(input, "exit") != 0) {
         getcwd(dir, 1000);
         printf("%s$ ", dir);
         fgets(buf, 1000, stdin);
         buf[strlen(buf) - 1] = 0;
         input = buf;
-        //printf("%s\n", input);
+        // SEMICOLON
         while (strchr(input, ';') != NULL) {
-        //  printf("Yah%s\n", input);
             strcpy(token, strsep(&input, ";"));
             token[strlen(token) - 1] = '\0';
             input++;
-        //    printf("%s + %sEND\n", input, token);
             char **args = parse_args(token, length);
-          //  printf("T%s\n", token);
             if (strcmp(args[0], "cd") == 0) {
                 if (*length == 0) {
                     chdir("/");
@@ -87,47 +85,61 @@ int main() {
             }
             //free_args(args);
         }
-        //printf("T%d\n", strchr(input, '>') != NULL);
-        //printf("%s\n", input);
         char temp[1000];
         strcpy(temp, input);
         char **args = parse_args(input, length);
-        //printf("Bs%s\n", args[0]);
-        //printf("T%d\n", strchr(temp, '>') != NULL);
         if (strcmp(args[0], "cd") == 0) {
             if (*length == 0) {
                 chdir("/");
             } else {
                 chdir(args[*length]);
             }
-        } else if (strchr(temp, '>') != NULL) {
-          //  printf("File %s\n", args[*length]);
-          //  umask(0);
-            int fd = open(args[*length], O_RDWR | O_CREAT, 0640);
-          //  printf("FD: %s\n", strerror(errno));
-            int backup = dup(1);
-            dup2(fd, 1);
-            int boo = 0;
-            int count = 0;
-
-            for (; count < *length; count++) {
-            //  printf("ThingT %d %d \n", count, *args[count] == '>');
-                if (*args[count] == '>') {
-                    boo = 1;
+        } else if (strchr(temp, '>') != NULL || strchr(temp, '<') != NULL) {
+            if (strchr(temp, '>') != NULL) {
+                int fd = open(args[*length], O_RDWR | O_CREAT, 0640);
+                int backup = dup(1);
+                dup2(fd, 1);
+                int boo = 0;
+                int count = 0;
+                for (; count < *length; count++) {
+                    if (*args[count] == '>') {
+                        boo = 1;
+                    }
+                    if (boo == 1) {
+                        args[count] = NULL;
+                    }
                 }
-                if (boo == 1) {
-                    args[count] = NULL;
+                int f = fork();
+                if (f) {
+                    wait(status);
+                } else {
+                    execvp(args[0], args);
                 }
-            }
-          //  printf("Thing %s \n", args[1]);
-            int f = fork();
-            if (f) {
-                wait(status);
+                dup2(backup, 1);
+                close(fd);
             } else {
-                execvp(args[0], args);
+                int fd = open(args[*length], O_RDONLY);
+                int backup = dup(0);
+                dup2(fd, 0);
+                int boo = 0;
+                int i = 0;
+                for (; i < *length; i++) {
+                    if (strcmp(args[i], "<") == 0) {
+                        boo = 1;
+                    }
+                    if (boo == 1) {
+                        args[i] = NULL;
+                    }
+                }
+                int f = fork();
+                if (f) {
+                    wait(status);
+                } else {
+                    execvp(args[0], args);
+                }
+                dup2(backup, 0);
+                close(fd);
             }
-            dup2(backup, 1);
-            close(fd);
         } else {
             int f = fork();
             if (f) {
